439d8b8fbb1b8db4ab6689323a31793c8e37dcae
Palukuru Charan Kumar Reddy <85541737+charankumar0002@users.noreply.github.com>
2024-05-19 23:51:15 +0530

    Adding router and lazy loading

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,8 +1,22 @@
+import { Route, Router } from "react-router-dom";
+import Header from "./components/header";
+import { Suspense, lazy } from "react";
+const Home = lazy(() => import ("./pages/Home/home"));
+const About = lazy(() => import ("./pages/About/about"));
 
 function App() {
   return (
    <>
-  
+<Router>
+  <Header/>
+  <main>
+    <Suspense>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      
+    </Suspense>
+  </main>
+</Router>
    </>
   );
 }
